7507695dcab681c9aace9f37ec4e27ba71e81221
Ray Sun <kartboy500@gmail.com>
2016-11-12 10:48:35 -0600

    Removed MicroStrategy from README

--- README.md
+++ README.md
@@ -11,9 +11,3 @@ https://github.com/Schmavery/facebook-chat-api
 * **Ben Cooper** - [bencooper222](https://github.com/bencooper222)
 * **Ray Sun** - [rsun0](https://github.com/rsun0)
 * **John Valin** - [johnvalin](https://github.com/johnvalin)
-
-
-## Acknowledgements
-### APIs
-#### Visualization SDK by MicroStrategy
-https://lw.microstrategy.com/msdz/MSDL/_CurrentGARelease/docs/projects/VisSDK_All/default.htm#topics/HTML5/API_Reference.htm
--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 var http = require('http');
 var login = require("facebook-chat-api");
-var ThreadID = 1142870729093699; //Thread ID for our groupchat
-var start = 1;
-var end = 500;
-var timestamp = 1478963082; //"2016-10-30T08:30:00+00:00";
-
 
 http.createServer(function (request, response) {
     response.writeHead(200, {
@@ -18,23 +13,23 @@ http.createServer(function (request, response) {
 
 var login = require("facebook-chat-api");
 
+var threadID = 100009779980369; // ThreadID between Ray and test account
+var start = 1;
+var end = 5;
+
 // Gets the thread history
 login({email: "i950770@mvrht.com", password: "uberhacks3.0"}, function callback (err, api) {
-    if(err) return console.error(err);
+    if (err) return console.error(err);
     
-    api.getThreadList(0, 4, 'inbox', function callback (err, arr) {
-        console.log(arr);
-    });
+    //api.getThreadList(0, 4, 'inbox', function callback (err, arr) {
+    //    console.log(arr);
+    //});
     
-    api.getThreadHistory('100002528536269', 1, 4, null, function callback (error, history) {
+    api.getThreadHistory(threadID, start, end, null, function callback (error, history) {
         if (error) console.error(error);
         
         for (var i = 0; i < history.length; i++) {
-            api.sendMessage(history[i].body, history[i].threadId);
+            api.sendMessage(history[i].body, threadID);
         }
     });
-    
-    //api.listen(function callback(err, message) {
-    //    api.sendMessage(message.body, message.threadID);
-    //});
 });
