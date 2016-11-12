ac2a4607e7d2cf24257263d833eb0fa23ec2441f
Ray Sun <kartboy500@gmail.com>
2016-11-12 09:28:56 -0600

    Used thread IDs

--- server.js
+++ server.js
@@ -17,8 +17,20 @@ var login = require("facebook-chat-api");
 // Create simple echo bot
 login({email: "i950770@mvrht.com", password: "uberhacks3.0"}, function callback (err, api) {
     if(err) return console.error(err);
-
-    api.listen(function callback(err, message) {
-        api.sendMessage(message.body, message.threadID);
+    
+    api.getThreadList(0, 4, 'inbox', function callback (err, arr) {
+        console.log(arr);
     });
+    
+    api.getThreadHistory('100002528536269', 1, 4, null, function callback (error, history) {
+        if (error) console.error(error);
+        
+        for (var i = 0; i < history.length; i++) {
+            api.sendMessage(history[i].body, history[i].threadId);
+        }
+    });
+    
+    //api.listen(function callback(err, message) {
+    //    api.sendMessage(message.body, message.threadID);
+    //});
 });
