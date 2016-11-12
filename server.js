88310b4203e603a6debfd3354e085e6462318423
Ray Sun <kartboy500@gmail.com>
2016-11-12 15:53:14 -0600

    Added function to get all history

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var login = require("facebook-chat-api");
+var natural = require('natural');
 
+// Create server
 http.createServer(function (request, response) {
     response.writeHead(200, {
         'Content-Type': 'text/plain',
@@ -9,27 +11,33 @@ http.createServer(function (request, response) {
     response.end('Hello World\n');
 }).listen(1337);
 
+// Initialize natural language classifier
+//classifier = new natural.BayesClassifier();
 
-
-var login = require("facebook-chat-api");
-
+// Get Messenger data
 var threadID = 100009779980369; // ThreadID between Ray and test account
-var start = 1;
-var end = 5;
-
-// Gets the thread history
 login({email: "i950770@mvrht.com", password: "uberhacks3.0"}, function callback (err, api) {
     if (err) return console.error(err);
+    
+    function getAllHistory(threadID, n, cb) {        
+        api.getThreadHistory(threadID, 1, n, null, function callback(error, history) {
+            if (history.length === n) getAllHistory(threadID, n * 10, cb);
+            else cb(error, history);
+        });
+    }
     
     //api.getThreadList(0, 4, 'inbox', function callback (err, arr) {
     //    console.log(arr);
     //});
     
-    api.getThreadHistory(threadID, start, end, null, function callback (error, history) {
+    getAllHistory(threadID, 100, function callback (error, history) {
         if (error) console.error(error);
         
+        console.log(history.length);
+        
         for (var i = 0; i < history.length; i++) {
-            api.sendMessage(history[i].body, threadID);
+            //api.sendMessage(history[i].body, threadID);
+            //console.log(history[i].body);
         }
     });
 });
