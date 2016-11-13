96a3568fc3a5e6543d7215c14373dc9cb084b395
Ray Sun <kartboy500@gmail.com>
2016-11-12 21:03:46 -0600

    Added separate bully counts for each sender

--- server.js
+++ server.js
@@ -33,24 +33,48 @@ login({email: testEmail, password: testPassword}, function callback (err, api) {
     }
     
     function checkConvo(classifier) {
-        getAllHistory(conversationID, 100, function callback (error, history) {
-            for (var i = 0; i < history.length; i++) {
-                if (history[i].body !== undefined) console.log(history[i].body, classifier.classify(history[i].body));
-            }
+        getAllHistory(conversationID, 100, function callback(error, history) {
+            api.getThreadInfo(conversationID, function cb(err, info) {
+                api.getUserInfo(info.participantIDs, function c(e, obj) {
+                    var bullyCounts = {};
+                    var totalCounts = {};
+                    
+                    for (var userID in obj) {
+                        bullyCounts[obj[userID].name] = 0;
+                        totalCounts[obj[userID].name] = 0;
+                    }
+                    
+                    for (var i = 0; i < history.length; i++) {
+                        if (history[i].body !== undefined) {
+                            var classification = classifier.classify(history[i].body);
+                            if (classification === 'bully') {
+                                bullyCounts[history[i].senderName]++;
+                            }
+                            totalCounts[history[i].senderName]++;
+                            console.log(history[i].senderName, ': ', history[i].body, ' - ', classification);
+                        }
+                    }
+                    
+                    for (var name in totalCounts) {
+                        console.log(name, ': ', bullyCounts[name], ' / ', totalCounts[name]);
+                    }
+                });
+            });
         });
     }
     
     // Initialize natural language classifier
     classifier = new natural.BayesClassifier();
     
-    getAllHistory(bullyID, 100, function callback (error, history) {
+    getAllHistory(bullyID, 100, function callback(error, history) {
         if (error) console.error(error);
         
         for (var i = 0; i < history.length; i++) {
             if (history[i].body !== undefined) {
+                // Non-bullying messages mistakenly entered in the bully data training set
                 if (history[i].body === 'yo' || history[i].body === '^^' || history[i].body === '?' || history[i].body === "It's Ben"
                         || history[i].body === "It's def not Arun" || history[i].body === "Who is lotaly" || history[i].body === "arun") {
-                    console.log('Ignored bully text: ', history[i].body);
+                    console.log('Ignored (in bully data training set): ', history[i].body);
                 }
                 else {
                     classifier.addDocument(history[i].body, 'bully');
@@ -58,16 +82,20 @@ login({email: testEmail, password: testPassword}, function callback (err, api) {
             }
         }
         
-        getAllHistory(notbullyID, 100, function cb (error, hist) {
+        getAllHistory(notbullyID, 100, function cb(error, hist) {
             if (error) console.error(error);
             
             for (var i = 0; i < hist.length; i++) {
-                if (hist[i].body !== undefined) classifier.addDocument(hist[i].body, 'notbully');
+                if (hist[i].body !== undefined) classifier.addDocument(hist[i].body, 'not bully');
             }
             
             classifier.train();
             
             checkConvo(classifier);
         });
+    });
+    
+    api.listen(function callback(error, message) {
+        // Check for bullying
     });
 });
