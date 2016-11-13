e226f5c189d673eee74dc63b0ae5209f736583f7
bencooper222 <bencooper222@gmail.com>
2016-11-12 19:40:17 -0600

    Added lastuser info

--- dynamodb_example.js
+++ dynamodb_example.js
@@ -1,32 +1,32 @@
 var AWS = require("aws-sdk"); // first do:  npm install aws-sdk
 
 AWS.config.update({
-  region: "us-east-1", // doesn't matter while it's local but I'm running this out of Virginia so will matter when we go live
-  endpoint: "http://localhost:8000" // if you have anything running on port 8000, dynamo will crash unless you specify it
+    region: "us-east-1", // doesn't matter while it's local but I'm running this out of Virginia so will matter when we go live
+    endpoint: "http://localhost:8000" // if you have anything running on port 8000, dynamo will crash unless you specify it
 });
 
 
 
 
-// RUN THIS CODE EXACTLY ONCE. UNO. UN. 一. BAD THINGS HAPPEN IF IT IS RUN SEVERAL TIMES
+// RUN THIS CODE SNIPPET EXACTLY ONCE. UNO. UN. 一. BAD THINGS HAPPEN IF IT IS RUN SEVERAL TIMES
 
 var dynamodb = new AWS.DynamoDB();
 
 var params = {
-    TableName : "Threads",
-    KeySchema: [       
-        { AttributeName: "ThreadID", KeyType: "HASH"},  //Partition key - I actually have no fucking clue what HASH means so just go with it
+    TableName: "Threads",
+    KeySchema: [
+        { AttributeName: "ThreadID", KeyType: "HASH" }, //Partition key - I actually have no fucking clue what HASH means so just go with it
     ],
-    AttributeDefinitions: [       
+    AttributeDefinitions: [
         { AttributeName: "participantIDs", AttributeType: "SS" },
-        { AttributeName: "name", AttributeType: "S"},
-        { AttributeName: "emoji", AttributeType:"S"},
-        { AttributeName: "nicknames", AttributeType:"M"},
-        { AttributeName: "color", AttributeType:"S"},
-        {AttributeType: "messages",AttributeType: "SS"} // SS might be incorrect. I hope it isn't. 
+        { AttributeName: "name", AttributeType: "S" },
+        { AttributeName: "emoji", AttributeType: "S" },
+        { AttributeName: "nicknames", AttributeType: "M" },
+        { AttributeName: "color", AttributeType: "S" },
+        { AttributeName: "lastInfo", AttributeType: "M" } { AttributeType: "messages", AttributeType: "SS" } // SS might be incorrect. I hope it isn't. 
     ],
-    ProvisionedThroughput: {       // I don't know what this means
-        ReadCapacityUnits: 5, 
+    ProvisionedThroughput: { // I don't know what this means
+        ReadCapacityUnits: 5,
         WriteCapacityUnits: 5
     }
 };
@@ -60,15 +60,15 @@ var table = "Threads"; // that's the name of the table in AWS
 
 
 var params = {
-    TableName:table,
-    Item:{
+    TableName: table,
+    Item: {
         "participantIDs": "FILL THIS IN WITH ACTUAL SHIT",
         "name": "SAME AS ABOVE",
         "emoji": "SAME AS ABOVE",
         "nicknames": "SAME AS ABOVE - make sure this is a MAP",
         "color": "SAME AS ABOVE",
-        "messages":"THIS IS A SET OF STRINGS IDK HOW THAT WORKS"
-           
+        "messages": "THIS IS A SET OF STRINGS IDK HOW THAT WORKS"
+
     }
 };
 
