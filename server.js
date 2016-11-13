7164913ffc2c4026696db3c60f2ca965264a5396
Ray Sun <kartboy500@gmail.com>
2016-11-13 01:00:42 -0600

    Used recursion

--- server.js
+++ server.js
@@ -32,57 +32,60 @@ login({email: "i1029456@mvrht.com", password: "uberhacks3.0"}, function callback
             else cb(error, history);
         });
     }
+    
+    function processThread(threadList, j) {
+        //gets all the history for each thread
+        getAllHistory(threadList[j].threadID, 10, function callback (error, history) {
+            if (error) console.error(error);
+            //console.log(history);
+	        var result = [];
+	        var name = history[0].senderName;
+	        var time = history[0].timestamp;
+	        for (var message in history) {
+		        if (history[message].senderName != name) {
+            			result.push({senderName: history[message].senderName, timestamp: history[message].timestamp, delta: history[message].timestamp-time, body: history[message].body});
+		        name = history[message].senderName;
+		        time = history[message].timestamp;
+		        }
+		        else {
+			        result.push({senderName: history[message].senderName, timestamp: history[message].timestamp, delta: null, body: history[message].body});
+		        }
+	        }
+            var docClient = new AWS.DynamoDB.DocumentClient();
+
+            var table = "Threads"; // that's the name of the table in AWS
+            var params = {
+                TableName: table,
+                Item: {
+                    "ThreadID": threadList[j].threadID, // string
+                    "participantIDs": threadList[j].participantIDs, // set of strings
+                    "name": threadList[j].name, // string
+                    "emoji": threadList[j].emoji, //string (no chars allowed)
+                    "nicknames": threadList[j].nicknames, // MAP of IDs to nicknames
+                    "color": threadList[j].color, // string #ffffff for example
+                    "lastInfo": {name : time}, // map of last user and last message
+                    "messages": result // set of jsons with messages
+                }
+            };
+
+            console.log("Adding a new item...");
+            docClient.put(params, function(err, data) {
+                if (err) {
+                    console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+                } else {
+                    console.log("Added item:", JSON.stringify(data, null, 2));
+                }
+            });
+        //console.log(result);
+        //put the results (items) in the table one by one
+        });
+        
+        if (j < threadList.length-1) {
+            processThread(threadList, j+1);
+        }
+    }
 
     api.getThreadList(start, end, 'inbox', function callback (error, threadList) {
-    	for(var j = 0; j < threadList.length; j++) {
-	        //gets all the history for each thread
-	        getAllHistory(threadList[j].threadID, end, function callback (error, history) {
-	            if (error) console.error(error);
-	            //console.log(history);
-		        var result = [];
-		        var name = history[0].senderName;
-		        var time = history[0].timestamp;
-		        for (var message in history) {
-			        if (history[message].senderName != name) {
-	            			result.push({senderName: history[message].senderName, timestamp: history[message].timestamp, delta: history[message].timestamp-time, body: history[message].body});
-			        name = history[message].senderName;
-			        time = history[message].timestamp;
-			        }
-			        else {
-				        result.push({senderName: history[message].senderName, timestamp: history[message].timestamp, delta: null, body: history[message].body});
-			        }
-		        }
-	            var docClient = new AWS.DynamoDB.DocumentClient();
-
-	            var table = "Threads"; // that's the name of the table in AWS
-	            console.log(threadList);
-	            console.log(j);
-	            console.log(threadList[j]);
-	            var params = {
-	                TableName: table,
-	                Item: {
-	                    "ThreadID": threadList[j].threadID, // string
-	                    "participantIDs": threadList[j].participantIDs, // set of strings
-	                    "name": threadList[j].name, // string
-	                    "emoji": threadList[j].emoji, //string (no chars allowed)
-	                    "nicknames": threadList[j].nicknames, // MAP of IDs to nicknames
-	                    "color": threadList[j].color, // string #ffffff for example
-	                    "lastInfo": {name : time}, // map of last user and last message
-	                    "messages": results // set of jsons with messages
-	                }
-	            };
-	
-	            console.log("Adding a new item...");
-	            docClient.put(params, function(err, data) {
-	                if (err) {
-	                    console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-	                } else {
-	                    console.log("Added item:", JSON.stringify(data, null, 2));
-	                }
-	            });
-	        //console.log(result);
-	        //put the results (items) in the table one by one
-            });
-	    }
+    	processThread(threadList, 0);
     });
 });
